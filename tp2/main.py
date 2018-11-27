8c698e7a44999b9465af55354b5c561184e5a9c2
r <r@R.local>
2018-11-27 09:11:33 -0300

    Arreglo unas cosas de conexiones

--- tp2/main.py
+++ tp2/main.py
@@ -1,9 +1,11 @@
+import pyArango
 from pyArango.connection import *
 import json
 
-conn = Connection(arangoURL="0.0.0.0:8529",username="root", password="root_passwd")
-if conn.hasDatabase('tp2'):
-    conn['tp2'].dropAllCollections()
+conn = Connection(username="root", password="tp2")
+if conn.hasDatabase("tp2"):
+    db=conn['tp2']
+    conn["tp2"].dropAllCollections()
 else:
     db = conn.createDatabase(name="tp2")
 
@@ -16,8 +18,9 @@ else:
 #  persona= db.createCollection(name="persona")
 #  direccion= db.createCollection(name="direccion")
 
-def createAndLoadCollection(db,collectionName,fileName):
-    collection= db.createCollection(name=collectionName)
+
+def createAndLoadCollection(db, collectionName, fileName):
+    collection = db.createCollection(name=collectionName)
     with open(fileName) as json_data:
         d = json.load(json_data)
         json_data.close()
@@ -26,12 +29,12 @@ def createAndLoadCollection(db,collectionName,fileName):
             doc.set(item)
             doc.save()
 
-createAndLoadCollection(db,'calle','../mockArango-Calle.txt')
-createAndLoadCollection(db,'barrio','../mockArango-Barrio.txt')
-createAndLoadCollection(db,'oficial','../mockArango-Oficial-Completo-V1.txt')
-createAndLoadCollection(db,'direccion','../mockArango-Dirección.txt')
+
+createAndLoadCollection(db, "calle", "../mockArango-Calle.txt")
+createAndLoadCollection(db, "barrio", "../mockArango-Barrio.txt")
+createAndLoadCollection(db, "oficial", "../mockArango-Oficial-Completo-V1.txt")
+createAndLoadCollection(db, "direccion", "../mockArango-Direccion.txt")
 #  createAndLoadCollection(db,'persona','../mockArango-Persona.txt')
-#  createAndLoadCollection(db,'superheroe','../mockArango-Superhéroe.txt')
+#  createAndLoadCollection(db,'superheroe','../mockArango-Superheroe.txt')
 #  createAndLoadCollection(db,'incidente','../mockArango-Incidente.txt')
 #  createAndLoadCollection(db,'estadosumario','../mockArango-EstadoSumario.txt')
-
