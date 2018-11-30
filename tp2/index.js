b64f95ea159e0716bb1b6a6d01ca7f59351e82b0
r <r@R.local>
2018-11-30 16:18:28 -0300

    Subo el script para cargar datos

---
+++ tp2/index.js
@@ -0,0 +1,70 @@
+const fs = require("fs");
+const Database = require('arangojs').Database;
+
+
+
+const db = new Database('http://root:tp2@127.0.0.1:8529');
+
+db.listDatabases().then(
+    (list) => {
+        var db = list.filter(item => item === 'tp2')
+        if (db.length === 0) {
+            createDatabaseTP();
+        } else {
+            dropAndCreate();
+        }
+    },
+    err => console.error('Failed to create database:', err)
+)
+
+const dropAndCreate = () => {
+    db.dropDatabase('tp2').then(
+        () => {
+            console.log('Database Dropped')
+            createDatabaseTP();
+        },
+        err => console.error('Failed to drop database:', err)
+    )
+}
+const createDatabaseTP = () => {
+    db.createDatabase('tp2').then(
+        () => {
+            console.log('Database created');
+            db.useDatabase('tp2');
+            createAllFiles()
+        },
+        err => console.error('Failed to create database:', err)
+    )
+}
+const createAllFiles = () => {
+    createAndLoadCollection("calle", "./datos/calle.json")
+    createAndLoadCollection("barrio", "./datos/barrio.json")
+    createAndLoadCollection("oficial", "./datos/Oficiales.json")
+    createAndLoadCollection("cerrado", "./datos/cerrado.json")
+    createAndLoadCollection("departamento", "./datos/Departamento.json")
+    createAndLoadCollection("asuntosInternos", "./datos/asuntosInternos.json")
+    createAndLoadCollection("habilidadSuperheroe", "./datos/habilidad Superheroe.json")
+    createAndLoadCollection("pendiente", "./datos/pendiente.json")
+    createAndLoadCollection("seguimiento", "./datos/seguimiento.json")
+    createAndLoadCollection("tipodesignacion", "./datos/tipodesignacion.json")
+    createAndLoadCollection("vinculo", "./datos/vinculo.json")
+    createAndLoadCollection('organizacionesdelictivas', './datos/OrganizacionesDelictivas.json')
+    createAndLoadCollection('incidentes', './datos/Incidentes.json')
+}
+
+
+const createAndLoadCollection = (collectionName, filename) => {
+    const content = fs.readFileSync(filename);
+    const jsonContent = JSON.parse(content);
+    collection = db.collection(collectionName)
+
+    collection.create().then(
+        () => {
+            collection.import(jsonContent).then(
+                result => console.log('Import complete:'),
+                err => console.error('Import failed:')
+            );
+        },
+        err => console.error('Failed to create collection:', err)
+    );
+}
